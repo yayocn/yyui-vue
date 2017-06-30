f64480e615a61f9207ba645d79705891d3153b61
yayo <yangyong@navercorp.com>
2017-06-30 17:48:49 +0800

    添加头部导航

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -4,56 +4,64 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    // 入口文件，path.resolve()方法，可以结合我们给定的两个参数最后生成绝对路径，最终指向的就是我们的index.js文件
-    entry: path.resolve(__dirname, '../src/main.js'),
-    // 输出配置
-    output: {
-        // 输出路径是 myProject/output/static
-        path: path.resolve(__dirname, '../dist/static'),
-        publicPath: 'static/',
-        filename: '[name].[hash].js',
-        chunkFilename: '[id].[chunkhash].js'
-    },
-    resolve: {
-        extensions: [' ', '.js', '.vue'],
-        alias: {
-            'Vue': 'vue/dist/vue.js'
-        }
-    },
-    module: {
+  // 入口文件，path.resolve()方法，可以结合我们给定的两个参数最后生成绝对路径，最终指向的就是我们的index.js文件
+  entry: path.resolve(__dirname, '../src/main.js'),
+  // 输出配置
+  output: {
+    // 输出路径是 myProject/output/static
+    path: path.resolve(__dirname, '../dist/static'),
+    publicPath: 'static/',
+    filename: '[name].[hash].js',
+    chunkFilename: '[id].[chunkhash].js'
+  },
+  resolve: {
+    extensions: [' ', '.js', '.vue'],
+    alias: {
+      'Vue': 'vue/dist/vue.js'
+    }
+  },
+  module: {
 
-        rules: [
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader?presets=es2015',
-                exclude: [
-                    /node_modules/
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader?presets=es2015',
+        exclude: [
+          /node_modules/
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: '../index.html',
-            template: path.resolve(__dirname, '../src/yyui.html'),
-            inject: true
-        }),
-        new webpack.LoaderOptionsPlugin({
-            options: {
-                yyui: {
-                    loaders: {
-                        js: 'babel-loader'
-                    }
-                },
-            }
-        })
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: ['file-loader']
+      }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: '../index.html',
+      template: path.resolve(__dirname, '../src/yyui.html'),
+      inject: true
+    }),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        yyui: {
+          loaders: {
+            js: 'babel-loader'
+          }
+        },
+      }
+    })
+  ]
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'Vue'
 import navbar from './components/navbar.vue'
+
+import '../lib/yyui/css/yyui-1.1.0.min.css'
+import './assets/css/common.css'
+import './assets/css/docs.css'
+import './assets/css/sidebar-scrollspy.css'
+import './assets/css/yyui-site.css'
+
 Vue.config.debug = true;//开启错误提示
+
 window.onload = function () {
   new Vue({
     el: '#yyui',
