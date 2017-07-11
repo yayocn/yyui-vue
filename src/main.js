b12cb7db617254e2fa4aed6cccbeb00691797a62
yayo <yangyong@navercorp.com>
2017-07-11 11:11:13 +0800

    更改yyui库

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import Vue from 'Vue'
 import App from './App.vue'
 
-import '../lib/yyui/css/yyui-1.1.0.css'
+import '../lib/yyui/css/yyui-1.1.2.css'
 import './assets/css/common.scss'
 import './assets/css/docs.scss'
 import './assets/css/sidebar-scrollspy.scss'
 import './assets/css/yyui-site.scss'
 
 import $ from './assets/js/jquery-vendor'
-import '../lib/yyui/js/yyui-1.1.0'
+import '../lib/yyui/js/yyui-1.1.2'
 import './assets/js/scrollspy'
 
 Vue.config.debug = true // 开启错误提示
