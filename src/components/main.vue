dd408982a2f03a6404c30aae22f67c559ef32904
yayo <yangyong@navercorp.com>
2017-07-13 11:14:21 +0800

    优化navbar border

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,13 +1,32 @@
 <template>
   <main>
-    <router-view></router-view>
+    <transition :name="transitionName">
+      <router-view keep-alive></router-view>
+    </transition>
   </main>
 </template>
 
 <script>
   import masthead from './common/masthead.vue'
+  import 'animate.css'
+
+  const animatesIn = ['bounceIn', 'bounceInDown', 'fadeIn', 'fadeInDown', 'flipInX', 'zoomIn', 'rollIn']
+  const animatesOut = ['bounceOut', 'bounceInDown', 'fadeOut', 'fadeInDown', 'flipOutX', 'zoomOut', 'rollOut']
 
   export default {
-    name: 'main'
+    name: 'main',
+    data () {
+      return {
+        transitionName: 'fade'
+      }
+    },
+    watch: {
+      $route () {
+//        let classIndex = Math.floor((Math.random() * animatesIn.length));
+//        this.inClass = animatesIn[classIndex];
+//        this.outClass = animatesOut[classIndex];
+        this.transitionName = 'bounce';
+      }
+    }
   }
 </script>
