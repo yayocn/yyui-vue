85dd14bd5f337198df034c6a1e203098cf1e39c8
yayo <yayocn@qq.com>
2017-07-26 10:07:53 +0800

    修复：切换路由时滚动条位置没变

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -4,8 +4,6 @@ export default {
   toAnchor (anchor) {
     var ele = $(`#${anchor}`)
     var top = ele.offset().top
-    $('body').animate({
-      scrollTop: top - 60
-    }, 500, 'easeInOutExpo')
+    $(window).scrollTop(top-60)
   }
 }
